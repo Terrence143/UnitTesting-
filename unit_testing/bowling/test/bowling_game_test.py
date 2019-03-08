eb65b0b43745a4ee159aae9427034e84d59bc528
Terrence <40508730+Terrence143@users.noreply.github.com>
2019-03-08 14:34:16 +0800

    Testing codes

---
+++ unit_testing/bowling/test/bowling_game_test.py
@@ -0,0 +1,45 @@
+import unittest
+from bowling.game import BowlingGame
+
+
+class BowlingGameTest(unittest.TestCase):
+
+    def setUp(self):
+        self.game = BowlingGame()
+
+    def testGutterGame(self):
+        self.rollMany(0, 20)
+        assert self.game.score() == 0
+
+    def testAllOnes(self):
+        self.rollMany(1, 20)
+        assert self.game.score()== 20
+
+    def testOneSpare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+        self.game.roll(3)
+        self.rollMany(0, 17)
+        assert self.game.score() == 16
+
+    def testOneStrike(self):
+        self.game.roll(10)
+        self.game.roll(3)
+        self.game.roll(4)
+        self.rollMany(0, 16)
+        assert self.game.score() == 24
+
+    def testPerfectGame(self):
+        self.rollMany(10, 12)
+        assert self.game.score() == 300
+
+    def testAllSpares(self):
+        self.rollMany(5, 21)
+        assert self.game.score() == 150
+
+    def rollMany(self, pins, rolls):
+        for i in range(rolls):
+            self.game.roll(pins)
+
+if __name__ == "__main__":
+    unittest.main()
